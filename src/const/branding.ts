6e2e5d3a7096a1c6ce3d792f23c1f760e8fc4936
Max <xd0718@hotmail.com>
2025-02-24 13:33:41 +0800

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -4,7 +4,7 @@
 
 export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = '聊城人工智能Ai医疗测试平台';
 export const BRANDING_LOGO_URL = '';
 
 export const ORG_NAME = 'LobeHub';
